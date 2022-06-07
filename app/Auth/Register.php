e251d71da3c4f073884ef397d945c9f94ed2fbd1
Rainaldo <61870393+Asociateone@users.noreply.github.com>
2022-06-07 10:44:59 +0200

    code styling

--- app/Auth/Register.php
+++ app/Auth/Register.php
@@ -1,74 +1,75 @@
-<?php 
-    namespace App\Auth;
+<?php
 
-    use App\Server\MySQL;
-    use Exception;
-    use App\Features\PasswordCheck;
-    use App\Features\StringCheck;
+namespace App\Auth;
 
-    class Register
+use App\Server\MySQL;
+use Exception;
+use App\Features\PasswordCheck;
+use App\Features\StringCheck;
+
+class Register
+{
+    private object $conn;
+
+    public function __construct()
     {
-        private object $conn;
+        $this->conn = new Mysql('localhost:8889', 'root', 'root');
+    }
 
-        public function __construct()
-        {
-            $this->conn = new Mysql('localhost:8889', 'root', 'root');
-        }
+    public function register(string $username, string $password, string $email): void
+    {
+        $this->conn->connect();
 
-        public function register(String $username, String $password, String $email): void
-        {
-            $this->conn->connect();
+        $username = StringCheck::inputTest($username);
+        $password = PasswordCheck::passwordCheck($password);
+        $email = StringCheck::inputTest($email);
 
-            $username = StringCheck::inputTest($username);
-            $password = PasswordCheck::passwordCheck($password);
-            $email = StringCheck::inputTest($email);
+        self::checkIfUsernameAlreadyExist($username);
+        self::checkIfEmailAlreadyExist($email);
 
-            self::checkIfUsernameAlreadyExist($username);
-            self::checkIfEmailAlreadyExist($email);
+        $arr = self::passwordHasher($password);
 
-            $arr = self::passwordHasher($password);
+        $query = "INSERT INTO password.Users (
+            `Username`, `Password`, `Email`, `Salt`
+            ) VALUES (
+                '$username', '$arr[password]', '$email', '$arr[salt]'
+            )";
 
-            $query = "INSERT INTO password.Users (
-                `Username`, `Password`, `Email`, `Salt`
-                ) VALUES (
-                    '$username', '$arr[password]', '$email', '$arr[salt]'
-                )";
+        $this->conn->sendQuery($query);
 
-            $this->conn->sendQuery($query);
+        $this->conn->disconnect();
+    }
 
-            $this->conn->disconnect();
-        }
+    private function passwordHasher(string $password): array
+    {
+        $salt = random_bytes(12);
+        $salt = bin2hex($salt);
 
-        private function passwordHasher(String $password): Array
-        {   
-            $salt = random_bytes(12);
-            $salt = bin2hex($salt);
+        $password .= $salt;
+        $password = password_hash($password, PASSWORD_DEFAULT);
 
-            $password .= $salt;
-            $password = password_hash($password, PASSWORD_DEFAULT);
+        return ['salt' => $salt, 'password' => $password];
+    }
 
-            return ['salt' => $salt, 'password' => $password];
-        }
+    private function checkIfUsernameAlreadyExist(string $username)
+    {
+        $query = "SELECT * FROM password.Users WHERE `Username` = '$username'";
 
-        private function checkIfUsernameAlreadyExist(String $username)
-        {
-            $query = "SELECT * FROM password.Users WHERE `Username` = '$username'";
+        $user = $this->conn->sendQuery($query);
 
-            $user = $this->conn->sendQuery($query);
-
-            if ($user->fetch_assoc()) {
-                throw new Exception('Username already exist');
-            }
-        }
-
-        private function checkIfEmailAlreadyExist(String $email): void
-        {
-            $query = "SELECT * FROM password.Users WHERE `Email` = '$email'";
-
-            $user = $this->conn->sendQuery($query);
-
-            if ($user->fetch_assoc()) {
-                throw new Exception('Email already exist');
-            }
+        if ($user->fetch_assoc()) {
+            throw new Exception('Username already exist');
         }
     }
+
+    private function checkIfEmailAlreadyExist(string $email): void
+    {
+        $query = "SELECT * FROM password.Users WHERE `Email` = '$email'";
+
+        $user = $this->conn->sendQuery($query);
+
+        if ($user->fetch_assoc()) {
+            throw new Exception('Email already exist');
+        }
+    }
+}
